3349a52585b305c21a98d158cad951b0c1429270
Yerkebulan <erkebulan777@gmail.com>
2022-08-19 21:55:22 +0600

    msgfix

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
 using RevitTimasBIMTools.RevitModel;
 using RevitTimasBIMTools.Services;
 using System;
--- Views/SettingsWindow.xaml.cs
+++ Views/SettingsWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RevitTimasBIMTools.Views
 {
@@ -29,6 +18,11 @@ namespace RevitTimasBIMTools.Views
             Properties.Settings.Default.Save();
             this.Close();
 
+        }
+
+        private void advanceViz_Checked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("YYYYES");
         }
     }
 }
