92e5c6b254e20891987f58bdadb6e8318c7a3484
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-08-23 13:53:36 +0600

    handler

--- Core/CutOpening/CutOpeningSettingsHandler.cs
+++ Core/CutOpening/CutOpeningSettingsHandler.cs
@@ -1,13 +1,89 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitTimasBIMTools.Services;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Document = Autodesk.Revit.DB.Document;
 
 namespace RevitTimasBIMTools.Core
 {
-    internal class CutOpeningSettingsHandler
+    internal class CutOpeningSettingsHandler : IExternalEventHandler
     {
+        public event EventHandler<SettingsCompletedEventArgs> Completed;
+        public void Execute(UIApplication uiapp)
+        {
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc?.Document;
 
+            if (doc == null)
+            {
+                return;
+            }
+        }
+
+
+        public IList<BuiltInCategory> GetFitrableCategories(Document document, Predicate<BuiltInCategory> collection)
+        {
+            List<BuiltInCategory> output = new List<BuiltInCategory>();
+            foreach (ElementId catId in ParameterFilterUtilities.GetAllFilterableCategories())
+            {
+                try
+                {
+                    Category category = Category.GetCategory(document, catId);
+                    if (category != null && category.AllowsBoundParameters)
+                    {
+                        if (category.CategoryType == CategoryType.Model)
+                        {
+                            output.Add((BuiltInCategory)catId.IntegerValue);
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    LogManager.Error(exc.Message);
+                }
+            }
+
+            return output.FindAll(collection);
+        }
+
+
+        private static bool FindComputer(BuiltInCategory bics)
+        {
+            //if (bk.Genre == "Computer")
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return false;
+        }
+
+
+        private void OnCompleted(SettingsCompletedEventArgs e)
+        {
+            Completed?.Invoke(this, e);
+        }
+
+
+        public string GetName()
+        {
+            return nameof(CutOpeningSettingsHandler);
+        }
+
+    }
+
+
+    public class SettingsCompletedEventArgs : EventArgs
+    {
+        public IList<Category> Categories { get; }
+        public IList<Element> Elements { get; }
+        public SettingsCompletedEventArgs(IList<Category> categories, IList<Element> elements)
+        {
+            Categories = categories;
+            Elements = elements;
+        }
     }
 }
