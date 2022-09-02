ab57686add6a2d6502c2205d86940eb22f76b9c5
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-09-02 19:54:34 +0600

    132

--- Core/SmartToolController.cs
+++ Core/SmartToolController.cs
@@ -30,11 +30,12 @@ namespace RevitTimasBIMTools.Core
         {
             IServiceCollection services = new ServiceCollection();
 
+            services = services.AddSingleton<CutOpeningRegisterDockablePane>();
+
             services = services.AddSingleton<IRevitTask, RevitTask>();
             services = services.AddSingleton<CutOpeningStartHandler>();
             services = services.AddSingleton<SmartToolGeneralHelper>();
-            services = services.AddSingleton<CutOpeningRegisterDockablePane>();
- 
+            
             services = services.AddTransient<IDockablePaneProvider, CutOpeningDockPanelView>();
 
             services = services.AddTransient<CutOpeningSettingsView>();
