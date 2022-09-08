d798acc4df71624741aeee7501bbce4d10f6d165
Yerkebulan <erkebulan777@gmail.com>
2022-09-08 22:45:32 +0600

    123

--- CustomControls/SliderControl.cs
+++ CustomControls/SliderControl.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace RevitTimasBIMTools.CustomControls
 {
     internal class SliderControl : Control
     {
-
+        public SliderControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderControl), new FrameworkPropertyMetadata(typeof(SliderControl)));
+        }
     }
+
 }
