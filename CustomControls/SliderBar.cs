c8f6ff640048485eb7b423fa33ad75bcfcc22f9d
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-09-21 15:14:22 +0600

    132

--- CustomControls/SliderBar.cs
+++ CustomControls/SliderBar.cs
@@ -3,11 +3,13 @@ using System.Windows.Controls;
 
 namespace RevitTimasBIMTools.CustomControls
 {
+    [TemplatePart(Name = SliderControl, Type = typeof(Slider))]
     [TemplatePart(Name = SliderBorder, Type = typeof(Border))]
     internal sealed class SliderBar : Slider
     {
+        public const string SliderControl = "PART_Slider";
         public const string SliderBorder = "PART_Border";
-
+        
         public string Content
         {
             get => (string)GetValue(ContentProperty);
@@ -18,10 +20,10 @@ namespace RevitTimasBIMTools.CustomControls
             DependencyProperty.Register("Content", typeof(string), typeof(SliderBar), new PropertyMetadata(""));
 
 
-        public SliderBar()
-        {
-            //DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderBar), new FrameworkPropertyMetadata(typeof(SliderBar)));
-        }
+        //public SliderBar()
+        //{
+        //    //DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderBar), new FrameworkPropertyMetadata(typeof(SliderBar)));
+        //}
 
 
         public CornerRadius CornerRadius
@@ -40,12 +42,17 @@ namespace RevitTimasBIMTools.CustomControls
             = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(SliderBar), new FrameworkPropertyMetadata(default(CornerRadius)));
 
 
+        public Slider sliderControl{ get; private set; }
         public Border sliderBorder { get; private set; }
 
         public override void OnApplyTemplate()
         {
+
             base.OnApplyTemplate();
+
+            sliderControl = GetTemplateChild(SliderControl) as Slider;
             sliderBorder = GetTemplateChild(SliderBorder) as Border;
+            sliderBorder.CornerRadius = CornerRadius;
         }
     }
 
