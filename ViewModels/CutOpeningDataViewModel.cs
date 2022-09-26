46a179ab3b617bdcd8d92db1462cfeb62169514c
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-09-26 19:15:43 +0600

    123

--- ViewModels/CutOpeningDataViewModel.cs
+++ ViewModels/CutOpeningDataViewModel.cs
@@ -134,7 +134,6 @@ namespace RevitTimasBIMTools.ViewModels
         }
 
 
-
         private ObservableCollection<ElementModel> modelCollection = new ObservableCollection<ElementModel>();
         public ObservableCollection<ElementModel> RevitElementModels
         {
@@ -332,7 +331,6 @@ namespace RevitTimasBIMTools.ViewModels
             }
         }
         #endregion
-
 
 
 
