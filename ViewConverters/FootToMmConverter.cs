ba27b0941f8e17b6fce4213de9a2585148ba1cde
Yerkebulan <erkebulan777@gmail.com>
2022-10-04 21:23:22 +0600

    convert

--- ViewConverters/FootToMmConverter.cs
+++ ViewConverters/FootToMmConverter.cs
@@ -9,12 +9,12 @@ namespace RevitTimasBIMTools.ViewConverters
         private const double footToMm = 12 * 25.4;
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is double val ? System.Convert.ToInt16(val * footToMm) : 0;
+            return value is double val ? System.Convert.ToInt16(val / footToMm) : 0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is int val ? System.Convert.ToDouble(val / footToMm) : 0;
+            return value is int val ? System.Convert.ToDouble(val * footToMm) : 0;
         }
     }
 }
