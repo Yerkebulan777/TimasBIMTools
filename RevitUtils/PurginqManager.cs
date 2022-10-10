a32a3b5ccefaa2d90650cbeff739111c4b2dbf6c
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-10-10 15:26:31 +0600

    123

--- RevitUtils/PurginqManager.cs
+++ RevitUtils/PurginqManager.cs
@@ -17,7 +17,6 @@ namespace RevitTimasBIMTools.RevitUtils
             };
 
 
-
             FilteredElementCollector collector;
 
             ElementMulticategoryFilter multiCat = new(purgeBuiltInCats);
