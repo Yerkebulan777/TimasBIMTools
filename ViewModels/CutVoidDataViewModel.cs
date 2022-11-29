f489a1b4c6738c38963199e893f0e23ae8bf781b
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-11-29 20:46:25 +0600

    12345

--- ViewModels/CutVoidDataViewModel.cs
+++ ViewModels/CutVoidDataViewModel.cs
@@ -543,9 +543,9 @@ namespace RevitTimasBIMTools.ViewModels
                     viewData.SortDescriptions.Clear();
                     viewData.GroupDescriptions.Clear();
                     viewData.GroupDescriptions.Add(new PropertyGroupDescription(nameof(ElementModel.FamilyName)));
-                    viewData.SortDescriptions.Add(new SortDescription(nameof(ElementModel.IsSelected), ListSortDirection.Ascending));
                     viewData.SortDescriptions.Add(new SortDescription(nameof(ElementModel.SymbolName), ListSortDirection.Ascending));
                     viewData.SortDescriptions.Add(new SortDescription(nameof(ElementModel.MinSizeInMm), ListSortDirection.Ascending));
+                    viewData.SortDescriptions.Add(new SortDescription(nameof(ElementModel.IsSelected), ListSortDirection.Descending));
                 }
             }
         }
