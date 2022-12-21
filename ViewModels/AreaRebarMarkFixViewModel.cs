d77597bed084a7628b55614e2f7e0cf15c56f35a
E.Jumabaev <erkebulan777@gmail.com>
2022-12-21 16:27:40 +0600

    1234567

--- ViewModels/AreaRebarMarkFixViewModel.cs
+++ ViewModels/AreaRebarMarkFixViewModel.cs
@@ -117,24 +117,24 @@ namespace RevitTimasBIMTools.ViewModels
         {
             await RevitTask.RunAsync(app =>
             {
+                map = null;
                 doc = app.ActiveUIDocument.Document;
                 foreach (Element element in areaReinforcements)
                 {
-                    if (param is not null && element is AreaReinforcement areaReinforce)
+                    if (map is null && element is AreaReinforcement areaReinforce)
                     {
                         IList<ElementId> rebarIds = areaReinforce.GetRebarInSystemIds();
                         TransactionManager.CreateTransaction(doc, "Set Mark", () =>
                         {
                             int counter = 0;
                             Random rnd = new();
-                            map = new Dictionary<string, ValueDataModel>();
                             while (0 < rebarIds.Count)
                             {
                                 counter++;
                                 Task.Delay(timeSpan);
                                 int index = rnd.Next(0, rebarIds.Count);
                                 Element elem = doc.GetElement(rebarIds[index]);
-                                if (elem is RebarInSystem rebarIn)
+                                if (param is not null && elem is RebarInSystem rebarIn)
                                 {
                                     Logger.Log($"All: {rebarIds.Count} Counter: {counter} Index: {index} IsStarted: {counter > limit}");
                                     if (ValidateParameter(param, rebarIn, counter > limit))
@@ -172,6 +172,7 @@ namespace RevitTimasBIMTools.ViewModels
                 {
                     Task.Delay(timeSpan);
                     Logger.Log("Set new parameter...");
+                    map ??= new Dictionary<string, ValueDataModel>();
                     map.Add(name, new ValueDataModel(value));
                 }
                 else
