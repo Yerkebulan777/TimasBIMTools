806cd8aa4079bf94f4aa347350f3e3a0c21d7dec
Yerkebulan <erkebulan777@gmail.com>
2022-12-22 02:11:08 +0600

    12345

--- ViewModels/AreaRebarMarkFixViewModel.cs
+++ ViewModels/AreaRebarMarkFixViewModel.cs
@@ -171,18 +171,15 @@ namespace RevitTimasBIMTools.ViewModels
                 Debug.Assert(!string.IsNullOrEmpty(model.Content), "Value can't be null\t" + msg);
                 isValidated = rebar.get_Parameter(paramGuid).SetValue(model.Content);
             }
-            else
+            if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
             {
-                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                if (!paramData.TryGetValue(name, out ValueDataModel data))
                 {
-                    if (!paramData.TryGetValue(name, out ValueDataModel data))
-                    {
-                        paramData.Add(name, new ValueDataModel(value));
-                    }
-                    else
-                    {
-                        data?.SetNewValue(value);
-                    }
+                    paramData.Add(name, new ValueDataModel(value));
+                }
+                else
+                {
+                    data?.SetNewValue(value);
                 }
             }
 
