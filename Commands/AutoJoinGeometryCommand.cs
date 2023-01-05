d36b5513f1ad1a6f82c0f270271fb8c603107ba2
E.Jumabaev <erkebulan777@gmail.com>
2023-01-05 18:12:14 +0600

    FixJoin

--- Commands/AutoJoinGeometryCommand.cs
+++ Commands/AutoJoinGeometryCommand.cs
@@ -4,7 +4,6 @@ using Autodesk.Revit.UI;
 using RevitTimasBIMTools.RevitUtils;
 using RevitTimasBIMTools.Services;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Document = Autodesk.Revit.DB.Document;
 using Line = Autodesk.Revit.DB.Line;
@@ -18,35 +17,49 @@ namespace RevitTimasBIMTools.Commands;
 internal sealed class AutoJoinGeometryCommand : IExternalCommand, IExternalCommandAvailability
 {
 
+    public const int MaxWitdhInMm = 50;
+
     Result IExternalCommand.Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
         UIApplication uiapp = commandData.Application;
         UIDocument uidoc = uiapp.ActiveUIDocument;
+        Level level = uidoc.ActiveView.GenLevel;
         Document doc = uidoc.Document;
+
+        if (level is null || !level.IsValidObject)
+        {
+            message = "Level not Valid";
+            return Result.Failed;
+        }
 
         int counter = 0;
         XYZ offset = new(0.1, 0.1, 0.1);
         FilteredElementCollector collector;
         BuiltInCategory bip = BuiltInCategory.OST_Walls;
-        IList<Element> allWalls = RevitFilterManager.GetElementsOfCategory(doc, typeof(Wall), bip, true).ToElements();
-        foreach (Wall wall1 in allWalls)
+        ElementLevelFilter level1Filter = new(level.Id);
+        double nativeWitdh = UnitUtils.ConvertToInternalUnits(MaxWitdhInMm, DisplayUnitType.DUT_MILLIMETERS);
+        collector = RevitFilterManager.GetElementsOfCategory(doc, typeof(Wall), bip, true);
+        foreach (Wall walltrg in collector.WherePasses(level1Filter).ToElements())
         {
-            if (wall1.FindInserts(true, true, true, true).Any())
+            if (walltrg.FindInserts(true, true, true, true).Any())
             {
-                BoundingBoxXYZ bb = wall1.get_BoundingBox(null);
-                ElementLevelFilter level1Filter = new(wall1.LevelId);
-                Outline outline = new Outline(bb.Min -= offset, bb.Max += offset);
+                BoundingBoxXYZ bb = walltrg.get_BoundingBox(null);
+                Outline outline = new(bb.Min -= offset, bb.Max += offset);
                 collector = RevitFilterManager.GetElementsOfCategory(doc, typeof(Wall), bip, true);
                 collector = collector.WherePasses(new BoundingBoxIntersectsFilter(outline));
-                foreach (Wall wall2 in collector.WherePasses(level1Filter).ToElements())
+                foreach (Wall wallsrs in collector.WherePasses(level1Filter).ToElements())
                 {
-                    if (JoinGeometryUtils.AreElementsJoined(doc, wall1, wall2)) { continue; }
+                    if (JoinGeometryUtils.AreElementsJoined(doc, walltrg, wallsrs)) { continue; }
 
-                    Line line1 = (wall1.Location as LocationCurve).Curve as Line;
-                    Line line2 = (wall2.Location as LocationCurve).Curve as Line;
+                    WallType wallType = doc.GetElement(wallsrs.GetTypeId()) as WallType;
 
-                    XYZ normal1 = line1.Direction.ToPositive();
-                    XYZ normal2 = line2.Direction.ToPositive();
+                    if (nativeWitdh < wallType.Width) { continue; }
+
+                    Line lineTrg = (walltrg.Location as LocationCurve).Curve as Line;
+                    Line lineSrs = (wallsrs.Location as LocationCurve).Curve as Line;
+
+                    XYZ normal1 = lineTrg.Direction.ToPositive();
+                    XYZ normal2 = lineSrs.Direction.ToPositive();
 
                     if (normal1.IsAlmostEqualTo(normal2))
                     {
@@ -56,16 +69,9 @@ internal sealed class AutoJoinGeometryCommand : IExternalCommand, IExternalComma
                         {
                             try
                             {
-                                XYZ point1 = line1.Evaluate(0.5, false);
-                                XYZ point2 = line2.Evaluate(0.5, false);
-                                double distance = point1.DistanceTo(point2);
-                                double maximum = (line1.Length + line2.Length) / 3;
-                                if (distance != 0 && distance < maximum)
-                                {
-                                    JoinGeometryUtils.JoinGeometry(doc, wall1, wall2);
-                                    status = trx.Commit();
-                                    counter++;
-                                }
+                                JoinGeometryUtils.JoinGeometry(doc, walltrg, wallsrs);
+                                status = trx.Commit();
+                                counter++;
                             }
                             catch (Exception ex)
                             {
@@ -85,7 +91,7 @@ internal sealed class AutoJoinGeometryCommand : IExternalCommand, IExternalComma
             }
         }
 
-        SBTLogger.Info($"Successfully Completed!\n Joined walls: {counter} count");
+        SBTLogger.Info($"Successfully Completed!\n Joined wallsrs: {counter} count");
         return Result.Succeeded;
     }
 
@@ -94,7 +100,7 @@ internal sealed class AutoJoinGeometryCommand : IExternalCommand, IExternalComma
     bool IExternalCommandAvailability.IsCommandAvailable(UIApplication uiapp, CategorySet selectedCategories)
     {
         View view = uiapp.ActiveUIDocument?.ActiveGraphicalView;
-        return view is ViewPlan or View3D;
+        return view is ViewPlan;
     }
 
 
